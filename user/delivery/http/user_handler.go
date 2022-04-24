48acf34732efe3a9314fe3da52582aa5560828d2
CO88 <blanccobb@gmail.com>
2022-04-24 23:10:32 +0900

    feat: user handler

---
+++ user/delivery/http/user_handler.go
@@ -0,0 +1,61 @@
+package http
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/CO88/go-ddd-boilerplate/user/domain"
+	"github.com/labstack/echo/v4"
+	"github.com/sirupsen/logrus"
+)
+
+type ResponseError struct {
+	Message string `json:"message"`
+}
+
+type UserHandler struct {
+	UserUsecase domain.UserUsecase
+}
+
+func NewUserHandler(e *echo.Echo, uu domain.UserUsecase) {
+	handler := &UserHandler{
+		UserUsecase: uu,
+	}
+
+	e.GET("/user/:id", handler.GetOneById)
+}
+
+func (uh *UserHandler) GetOneById(c echo.Context) error {
+	idp, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		return c.JSON(http.StatusNotFound, domain.ErrNotFound.Error())
+	}
+
+	id := int64(idp)
+	ctx := c.Request().Context()
+
+	user, err := uh.UserUsecase.GetOneById(ctx, id)
+	if err != nil {
+		return c.JSON(getStatusCode(err), ResponseError{Message: err.Error()})
+	}
+
+	return c.JSON(http.StatusOK, user)
+}
+
+func getStatusCode(err error) int {
+	if err == nil {
+		return http.StatusOK
+	}
+
+	logrus.Error(err)
+	switch err {
+	case domain.ErrInternalServerError:
+		return http.StatusInternalServerError
+	case domain.ErrNotFound:
+		return http.StatusNotFound
+	case domain.ErrConflict:
+		return http.StatusConflict
+	default:
+		return http.StatusInternalServerError
+	}
+}
