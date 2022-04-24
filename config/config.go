278b819a9eafbeb7ad1aa704b433e65d969be15a
CO88 <blanccobb@gmail.com>
2022-04-24 23:09:43 +0900

    feat: add config

---
+++ config/config.go
@@ -0,0 +1,52 @@
+package config
+
+import (
+	"log"
+
+	"github.com/spf13/viper"
+)
+
+type DBInfo struct {
+	Host         string `mapstructure:"hostname"`
+	Port         int    `mapstructure:"port"`
+	Name         string `mapstructure:"dbname"`
+	UserName     string `mapstructure:"username"`
+	UserPassword string `mapstructure:"password"`
+}
+
+type Context struct {
+	Timeout int
+}
+
+type Server struct {
+	Address string
+}
+
+type Configuration struct {
+	Db      DBInfo `mapstructure:"DB"`
+	Context Context
+	Server  Server
+}
+
+func LoadConfig() *Configuration {
+	viper.SetConfigName("config")
+	viper.SetConfigType("toml")
+	viper.AddConfigPath(".")
+	viper.AutomaticEnv()
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		log.Fatal("Config read error")
+		panic(err)
+	}
+
+	config := &Configuration{}
+
+	err = viper.Unmarshal(&config)
+	if err != nil {
+		log.Fatal("config unmarshal error")
+		panic(err)
+	}
+
+	return config
+}
