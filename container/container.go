1f7d98586affca2f648f6e9fafb1449f98b6d853
cobb.onuii <blanccobb@gmail.com>
2022-07-10 02:05:14 +0900

    fix: container를 사용하여 module을 받을 수 있게 수정

--- container/container.go
+++ container/container.go
@@ -1,35 +1,26 @@
 package container
 
 import (
-	"database/sql"
-	"time"
-
 	"github.com/CO88/go-ddd-boilerplate/api"
-	"github.com/CO88/go-ddd-boilerplate/user/delivery/http"
-	"github.com/CO88/go-ddd-boilerplate/user/repository/mysql"
-	"github.com/CO88/go-ddd-boilerplate/user/usecase"
+	"github.com/CO88/go-ddd-boilerplate/config"
+	user "github.com/CO88/go-ddd-boilerplate/user/delivery/http"
 )
 
-// type DIContainer struct {
-// 	Config   *config.Configuration
-// 	Handlers []api.ApiHandler
-// }
+type DIContainer struct {
+	Config   *config.Configuration
+	Handlers []api.ApiHandler
+}
 
-// var Container *DIContainer
+var Container *DIContainer
 
-// func NewContainer(dbConn *sql.DB, timeoutContext time.Duration) api.ApiHandler {
-// 	panic(wire.Build(
-// 		mysql.NewUserRepository,
-// 		usecase.NewUserUsecase,
-// 		http.NewUserHandler,
-// 	))
-// }
+func NewDIContainer(
+	configuration *config.Configuration,
+	userHanlder *user.UserHandler,
+) *DIContainer {
+	Container := &DIContainer{
+		configuration,
+		[]api.ApiHandler{userHanlder},
+	}
 
-func NewContainer(dbConn *sql.DB, timeout time.Duration) api.ApiHandler {
-
-	userRepo := mysql.NewUserRepository(dbConn)
-	userUsecase := usecase.NewUserUsecase(userRepo, timeout)
-	userHandler := http.NewUserHandler(userUsecase)
-
-	return userHandler
+	return Container
 }
